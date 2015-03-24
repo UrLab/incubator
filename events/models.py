9fcdf1da1e40f06a58a43040a5ce2d42a0666a48
Romain Fontaine <romain-fontaine@outlook.com>
2015-03-24 21:39:00 +0100

    [fill] fill events model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+#Events :
+#    Des users peuvent participer à un event
+#    Les gens peuvnet être "intéressés"
+#    Utiliser https://github.com/thoas/django-sequere ?
+#    API hackeragenda
+
+class Event(models.Model):
+    STATUS_CHOICES = (
+        ("i", "in preparation"),
+        ("r", "ready"),
+        ("p", "planned"),
+        ("j", "just an idea"),
+    )
+    place = models.CharField(max_length=300)
+    start = models.DateTimeField()
+    stop = models.DateTimeField()
+    title = models.CharField(max_length=300)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    organizer = models.OneToOneField(settings.AUTH_USER_MODEL)
