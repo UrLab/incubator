0467ce3a5f15dcbce4b6baeb6aced687356c2338
Romain Fontaine <romain-fontaine@outlook.com>
2015-03-24 22:24:57 +0100

    [add] Added user and balance field for an Hacker

--- hackers/models.py
+++ hackers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#    User :
+#    Des users ont des skills/badges
+#    Les badges ne peuvent être donnés que par quelqu'un qui l'a déjà (genre des teachers)
+#    un badge pourrait être "utilisateur de la reprap" et "certigfierait" que le user sait utiliser la machine
+#    Des users appartiennent à un groupe (anon, registered, membres cotisants, "bureau")
+#    Système d'emprunt (optionnel)
+
+class Hacker(models.Model):
+    user = models.OneToOneField(User)
+    balance = models.IntegerField(default=0)
+
