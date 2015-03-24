4b7466e3798dea0b3edf94c1e5cc376ba7615d2f
Romain Fontaine <romain-fontaine@outlook.com>
2015-03-25 00:50:17 +0100

    [add] Add a description to an event

--- events/models.py
+++ events/models.py
@@ -21,3 +21,5 @@ class Event(models.Model):
     title = models.CharField(max_length=300)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     organizer = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.TextField()
+
