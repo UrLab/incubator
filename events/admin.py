94667f1df2025fab05f40d6adbf0da2d628180fd
Nikita Marchant <nikita.marchant@gmail.com>
2015-10-08 12:04:18 +0200

    Add admin for events

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Meeting
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'start', 'stop', 'place', 'organizer')
+    list_filter = ('status', 'start', 'stop')
+    search_fields = ('title', 'description', 'place')
+
+
+@admin.register(Meeting)
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start', 'organizer')
+    list_filter = ('start',)
+    search_fields = ('OJ', 'PV', 'title')
