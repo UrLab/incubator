75c4f14990f7eb7a40d0ccbe310b58ac6018e382
Nikita Marchant <nikita.marchant@gmail.com>
2015-10-08 12:22:24 +0200

    Add admin to hackers

--- hackers/admin.py
+++ hackers/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hacker, MacAdress
+
+
+@admin.register(Hacker)
+class HackerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')
+    # list_filter = ('balance',)
+    search_fields = ('user',)
+
+
+@admin.register(MacAdress)
+class MacAdressAdmin(admin.ModelAdmin):
+    list_display = ('adress', 'holder')
+    # list_filter = ('adress',)
+    search_fields = ('adress', 'holder')
