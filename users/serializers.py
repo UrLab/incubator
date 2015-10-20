6116f76ac8e8f7061abab0d364f1cde944b4aa31
Nikita Marchant <nikita.marchant@gmail.com>
2015-10-20 13:51:10 +0200

    Fix user api

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,9 @@
-from django.contrib.auth.models import User
 from .models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-        fields = ('balance',)
-
-
-#class UserSerializer(serializers.HyperlinkedModelSerializer):
-#    hacker = HackerSerializer(many=False, read_only=False)
-#
-#    class Meta:
-#        model = User
-#        fields = ('id', 'username', 'email', 'hacker')
+        fields = ('username', 'balance', 'email', 'first_name', 'last_name', 'created', 'last_login')
