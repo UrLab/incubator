0025abc03423591b07520c993b09aa38c9577863
Nikita Marchant <nikita.marchant@gmail.com>
2015-10-20 18:05:35 +0200

    Add markdown support

---
+++ incubator/templatetags/formatting.py
@@ -0,0 +1,23 @@
+import re
+import markdown
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe, SafeText
+
+register = template.Library()
+
+
+@register.filter(is_safe=False, name='markdown')
+@stringfilter
+def my_markdown(value):
+    extensions = ["nl2br", "extra", "codehilite", "headerid(level=2)", "sane_lists"]
+
+    html = mark_safe(markdown.markdown(
+        value,
+        extensions,
+        safe_mode='escape',
+        enable_attributes=False,
+        output_format="html5"
+    ))
+    return SafeText(html)
