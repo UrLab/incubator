19a8a676d06ed7f252c9d956afbac78d359b0fc7
Nikita Marchant <nikita.marchant@gmail.com>
2015-11-03 14:47:56 +0100

    Macadress admin

--- space/admin.py
+++ space/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MacAdress
 
-# Register your models here.
+
+@admin.register(MacAdress)
+class MacAdressAdmin(admin.ModelAdmin):
+    list_display = ('adress', 'holder')
+    list_filter = ('hidden',)
+    search_fields = ('adress', 'holder')
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User# , MacAdress
+from .models import User
 
 
 @admin.register(User)
@@ -8,10 +8,3 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'balance', 'email', 'has_key', 'is_staff', 'is_member')
     list_filter = ('balance', 'has_key', 'is_staff', )
     search_fields = ('username',)
-
-
-# @admin.register(MacAdress)
-# class MacAdressAdmin(admin.ModelAdmin):
-#     list_display = ('adress', 'holder')
-#     # list_filter = ('adress',)
-#     search_fields = ('adress', 'holder')
