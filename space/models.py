7e699a0368976882f57bd9affa817142d10233ad
Romain Fontaine <romain-fontaine@outlook.com>
2015-11-06 11:41:28 +0100

    Lower the macadress before saving it

--- space/models.py
+++ space/models.py
@@ -7,6 +7,10 @@ class MacAdress(models.Model):
     machine_name = models.CharField(blank=True, max_length=100)
     holder = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        self.adress = self.adress.lower()
+        super(MacAdress, self).save(*args, **kwargs)
+
 
 class SpaceStats(models.Model):
     adress_count = models.IntegerField()
