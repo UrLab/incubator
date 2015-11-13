4dbe62c0b91f7f31216923dca07701dd99b0f101
Nikita Marchant <nikita.marchant@gmail.com>
2015-11-13 17:17:15 +0100

    Project description should not be required

--- projects/models.py
+++ projects/models.py
@@ -33,7 +33,7 @@ class Project(models.Model):
     picture = models.ImageField(upload_to='project_pictures', null=True, blank=True)
 
     short_description = models.CharField(max_length=1000, verbose_name="Description courte")
-    content = models.TextField(verbose_name="Contenu")
+    content = models.TextField(verbose_name="Contenu", blank=True)
 
     class Meta:
         verbose_name = "Projet"
