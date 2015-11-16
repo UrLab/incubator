48a4192c060331db69acdcd7c239f08aff96892e
Nikita Marchant <nikita.marchant@gmail.com>
2015-11-16 09:29:07 +0100

    More fields in user admin

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'balance', 'email', 'has_key', 'is_staff', 'is_member')
-    list_filter = ('balance', 'has_key', 'is_staff', )
-    search_fields = ('username',)
+    list_display = ('username', 'balance', 'email', 'has_key', 'is_staff', 'is_member', 'created')
+    list_filter = ('balance', 'has_key', 'is_staff', 'created')
+    search_fields = ('username', 'email')
