880121fd671662a3b2bb170cf0b1cb04f68f4990
iTitou <moiandme@gmail.com>
2015-11-27 14:44:31 +0100

    [enh] Users can change their passwords + password field display in admin
    STILL TODO: admin can change user passwords

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import auth
 
 from .models import User
 
@@ -8,3 +9,6 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'balance', 'email', 'has_key', 'is_staff', 'is_member', 'created')
     list_filter = ('balance', 'has_key', 'is_staff', 'created')
     search_fields = ('username', 'email')
+
+    add_form = auth.forms.UserCreationForm
+    form = auth.forms.UserChangeForm
