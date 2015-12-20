1302f4630c98e465ad01d5c9212255f18bd291f4
iTitou <moiandme@gmail.com>
2015-12-20 15:29:47 +0100

    [fix] Composition order

--- users/decorators.py
+++ users/decorators.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 
 
 def no_troll_required(func):
-    func = login_required(func)
-
     def wrapper(request, *args, **kwargs):
         u = request.user
         if not (u.has_key or u.is_staff) and u.is_troll:
             return render(request, "troll.html")
         return func(request, *args, **kwargs)
-    return wrapper
+    return login_required(wrapper)
