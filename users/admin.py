881c1c8b40df6b3d7b05669f08d55862d42aef68
iTitou <moiandme@gmail.com>
2015-12-20 23:44:25 +0100

    [add] List groups in User admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 from django.contrib import auth
 
@@ -6,8 +7,20 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    def groups(self, user):
+        short_name = str
+        p = sorted(
+            u"<a title='%s'>%s</a>" % (x, short_name(x))
+            for x in user.groups.all())
+        if user.user_permissions.count():
+            p += ['<strong>+</strong>']
+        value = ', '.join(p)
+        return mark_safe("%s" % value)
+    groups.allow_tags = True
+    groups.short_description = u'Membre des groupes'
+
     list_display = ('username', 'balance', 'email', 'has_key', 'is_superuser',
-                    'is_member', 'created')
+                    'is_member', 'created', 'groups')
     list_filter = ('balance', 'has_key', 'is_superuser', 'created')
     search_fields = ('username', 'email')
 
