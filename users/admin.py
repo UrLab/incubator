24b26e7050d5caed5943068c7140980594751e8b
iTitou <moiandme@gmail.com>
2015-12-20 15:26:47 +0100

    [add] Admin group action to mark user as troll or not

--- users/admin.py
+++ users/admin.py
@@ -6,6 +6,16 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    def not_a_troll(self, request, queryset):
+        queryset.update(is_troll=False)
+    not_a_troll.short_description = "Marquer comme NON-troll"
+
+    def a_troll(self, request, queryset):
+        queryset.update(is_troll=True)
+    a_troll.short_description = "Marquer comme troll"
+
+    actions = [not_a_troll, a_troll]
+
     list_display = ('username', 'balance', 'email', 'has_key', 'is_troll',
                     'is_staff', 'is_member', 'created')
     list_filter = ('balance', 'has_key', 'is_staff', 'created', 'is_troll')
