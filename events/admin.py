2f8425b62ea0c1f188c7842324b94d284b125e65
Nikita Marchant <nikita.marchant@gmail.com>
2016-01-02 21:35:13 +0100

    InlineForms, Fieldsets and even custom list filters o/ (also change some Meta on models so there is a migration)

--- events/admin.py
+++ events/admin.py
@@ -3,14 +3,23 @@ from django.contrib import admin
 from .models import Event, Meeting
 
 
+class MeetingInline(admin.StackedInline):
+    model = Meeting
+    filter_horizontal = ('members',)
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'start', 'stop', 'place', 'organizer')
     list_filter = ('status', 'start', 'stop')
     search_fields = ('title', 'description', 'place')
 
+    filter_horizontal = ('interested',)
+    inlines = (MeetingInline,)
+
 
 @admin.register(Meeting)
 class MeetingAdmin(admin.ModelAdmin):
     list_display = ('event',)
     search_fields = ('OJ', 'PV')
+    filter_horizontal = ('members',)
--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,46 @@ from django.contrib import admin
 from django.contrib import auth
 
 from .models import User
+from space.models import MacAdress
+
+
+class MacAdressInline(admin.TabularInline):
+    model = MacAdress
+    extra = 1
+
+
+class BalanceListFilter(admin.SimpleListFilter):
+    title = "Ardoise"
+    parameter_name = 'balance'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('debt', "Moins de -5€"),
+            ('neg5', "[-5€, 0€["),
+            ('z', '0€'),
+            ('pos5', "]0€, 5€["),
+            ('money', "Plus de 5€"),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'debt':
+            return queryset.filter(balance__lt=-5)
+        if self.value() == 'neg5':
+            return queryset.filter(
+                balance__gte=-5,
+                balance__lt=0,
+            )
+
+        if self.value() == 'z':
+            return queryset.filter(balance=0)
+
+        if self.value() == 'pos5':
+            return queryset.filter(
+                balance__gt=0,
+                balance__lte=5,
+            )
+        if self.value() == 'money':
+            return queryset.filter(balance__gt=5)
 
 
 @admin.register(User)
@@ -21,8 +61,24 @@ class UserAdmin(admin.ModelAdmin):
 
     list_display = ('username', 'balance', 'email', 'has_key', 'is_superuser',
                     'is_member', 'created', 'groups')
-    list_filter = ('balance', 'has_key', 'is_superuser', 'created')
-    search_fields = ('username', 'email')
+    list_filter = (BalanceListFilter, 'has_key', 'is_superuser', 'created', 'last_login')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    inlines = (MacAdressInline,)
+    filter_horizontal = ('groups', 'user_permissions')
+
+    fieldsets = (
+        (None, {
+            'fields': (('username', 'email'), ('first_name', 'last_name'), ('has_key', 'hide_pamela'), 'balance')
+        }),
+        (None, {
+            'fields': ('password', 'last_login')
+        }),
+        ('Permissions', {
+            'classes': ('collapse',),
+            'fields': ('is_superuser', 'groups', 'user_permissions')
+        }),
+    )
 
     add_form = auth.forms.UserCreationForm
     form = auth.forms.UserChangeForm
