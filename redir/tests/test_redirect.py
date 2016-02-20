9ede608c5dcc524dee8d4e17e29d1e21c1968070
iTitou <moiandme@gmail.com>
2016-02-20 17:48:35 +0100

    [fix] Remove example code

--- redir/tests/test_redirect.py
+++ redir/tests/test_redirect.py
@@ -3,15 +3,6 @@ from django.test import Client
 from redir.models import Redirection
 import pytest
 
-# >>> from django.test import Client
-# >>> c = Client()
-# >>> response = c.post('/login/', {'username': 'john', 'password': 'smith'})
-# >>> response.status_code
-# 200
-# >>> response = c.get('/customer/details/')
-# >>> response.content
-# b'<!DOCTYPE html...'
-
 
 @pytest.fixture(scope='function')
 def redir():
