f1681c837e794271fce5b3172c67209a2f2fb8e1
iTitou <moiandme@gmail.com>
2016-03-04 13:28:07 +0100

    [add] Opt-out instructions in mail footer

--- manmail/models.py
+++ manmail/models.py
@@ -3,7 +3,12 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 import markdown
 
+
 class Email(models.Model):
+    FOOTER = "_Si vous ne souhaitez pas recevoir cette newsletter," +\
+             " changez vos préférences sur " +\
+             "[votre profil utilisateur](https://urlab.be/accounts/edit)_"
+
     subject = models.CharField(max_length=511, verbose_name="Sujet")
     content = models.TextField(blank=True, verbose_name="Contenu")
     approvers = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, verbose_name="Approbateurs", related_name="approved_emails")
@@ -12,8 +17,9 @@ class Email(models.Model):
     modified = models.DateTimeField(auto_now=True)
 
     def markdown_content(self):
+        content = self.content + "\n" + self.FOOTER
         return mark_safe(markdown.markdown(
-            self.content,
+            content,
             ["nl2br"],
             safe_mode='escape',
             enable_attributes=False,
