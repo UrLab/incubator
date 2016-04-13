ebf0d3b9045e56cfab3ca6e03f97cd150b09a865
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-13 09:48:20 +0200

    Upgrade django and actstream

--- projects/apps.py
+++ projects/apps.py
@@ -1,11 +1,11 @@
 # myapp/apps.py
 from django.apps import AppConfig
-from actstream import registry
 
 
 class ProjectsConfig(AppConfig):
     name = 'projects'
 
     def ready(self):
+        from actstream import registry
         registry.register(self.get_model('Project'))
         registry.register(self.get_model('Task'))
