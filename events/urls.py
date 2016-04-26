bef158f7b5b4febdf733b2f2e425a1ec7d0b3ead
iTitou <moiandme@gmail.com>
2016-04-26 17:05:41 +0200

    [fix] Terminate url matching

--- events/urls.py
+++ events/urls.py
@@ -15,8 +15,8 @@ urlpatterns = patterns(
     url(r'^import_pad/(?P<pk>[0-9]+)$', import_pad, name='import_pad'),
     url(r'^export_pad/(?P<pk>[0-9]+)$', export_pad, name='export_pad'),
     url(r'^(?P<pk>[0-9]+)', EventDetailView.as_view(), name='view_event'),
-    url(r'^add_meeting/(?P<pk>[0-9]+)', MeetingAddView.as_view(), name='add_meeting'),
-    url(r'^edit_meeting/(?P<pk>[0-9]+)', MeetingEditView.as_view(), name='edit_meeting'),
+    url(r'^add_meeting/(?P<pk>[0-9]+)$', MeetingAddView.as_view(), name='add_meeting'),
+    url(r'^edit_meeting/(?P<pk>[0-9]+)$', MeetingEditView.as_view(), name='edit_meeting'),
     url(r'^not_interested/(?P<pk>[0-9]+)$', login_required(not_interested), name='not_interested_event'),
     url(r'^interested/(?P<pk>[0-9]+)$', login_required(interested), name='interested_event'),
 
--- space/urls.py
+++ space/urls.py
@@ -6,9 +6,9 @@ from .views import (pamela_list, status_change, DeleteMACView, motd_change,
 
 urlpatterns = patterns(
     '',
-    url(r'^pamela', pamela_list, name='pamela_list'),
-    url(r'^change_status', status_change, name='change_status'),
-    url(r'^change_motd', motd_change, name='change_motd'),
-    url(r'^remove_mac/(?P<pk>[0-9]+)', login_required(DeleteMACView.as_view()), name="delete_mac"),
-    url(r'^openings', openings, name='openings_graph'),
+    url(r'^pamela$', pamela_list, name='pamela_list'),
+    url(r'^change_status$', status_change, name='change_status'),
+    url(r'^change_motd$', motd_change, name='change_motd'),
+    url(r'^remove_mac/(?P<pk>[0-9]+)$', login_required(DeleteMACView.as_view()), name="delete_mac"),
+    url(r'^openings$', openings, name='openings_graph'),
 )
