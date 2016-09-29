843f9d3c4d517cabb0b98e4f01c83e4ce35bc69b
Nikita Marchant <nikita.marchant@gmail.com>
2016-09-29 16:20:11 +0200

    Fix #76

--- realtime/models.py
+++ realtime/models.py
@@ -4,6 +4,7 @@ from actstream.models import Action
 from realtime.helpers import send_message
 from django.conf import settings
 from wiki.models import ArticleRevision
+from actstream import action
 
 
 @receiver(post_save, sender=Action)
@@ -55,3 +56,5 @@ def wiki_save_handler(sender, created, instance, **kwargs):
         title=instance.title,
         url=url
     )
+    # Add an actream line
+    action.send(instance.user, verb='a édité', action_object=instance.article)
