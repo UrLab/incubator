c4192f0f6fbfb39f1db5963b2683a120a62c0b07
Pavel Polinetskyi <pavel.polinetskyi@gmail.com>
2017-09-20 11:57:41 +0200

    [add] New transaction models

--- stock/models.py
+++ stock/models.py
@@ -12,3 +12,29 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category)
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey("users.User")
+    when = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class TransferTransaction(Transaction):
+    user = models.ForeignKey("users.User", related_name="outgoing_transfers")
+    receiver = models.ForeignKey("users.User", related_name="incoming_transfers")
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class TopupTransaction(Transaction):
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class ProductTransaction(Transaction):
+    product = models.ForeignKey("stock.Product")
+
+
+class MiscTransaction(Transaction):
+    info = models.TextField()
