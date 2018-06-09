16a5218247ebed5521d52f36921f2371ad5f57ac
Pavel Polinetskyi <pavel.polinetskyi@gmail.com>
2018-06-09 15:50:34 +0200

    [add] Base models for stock tracking

--- stock/models.py
+++ stock/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -12,9 +13,47 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category)
+    stock_amount = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+
+class Barcode(models.Model):
+    product = models.ForeignKey("stock.Product")
+    code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.code
+
+
+class StockRefill(models.Model):
+    """
+    Represents a stock refilling event. Tracks who and when refilled the stock, uses ProductRefill in order
+        to track each individual stock that was updated.
+    """
+    user = models.ForeignKey("users.User", null=True, blank=True, on_delete=models.SET_NULL,
+                             help_text="The person responsible for this stock refilling. \
+                             Ideally the person who did the data entry after shopping")
+    when = models.DateTimeField(default=timezone.now)
+    updates = models.ManyToManyField(to="stock.Product", through="stock.ProductRefill",
+                                     help_text="All the product stocks that were updated in this refill")
+
+    def __str__(self):
+        return "Stock refilled by {} on {}".format(self.user, self.when)
+
+
+class ProductRefill(models.Model):
+    """
+    Many to many intermediary table used in order to store the product stock being updated with its quantity
+    Regrouped in StockRefill.
+    """
+    refill = models.ForeignKey("stock.StockRefill", help_text="The refill that generated this update")
+    product = models.ForeignKey("stock.Product", help_text="The product whose stock was refilled")
+    amount = models.IntegerField(help_text="The quantity by which the stock was increased")
+
+    def __str__(self):
+        return "Refill of {} by {} unit(s)".format(self.product, self.amount)
 
 
 class Transaction(models.Model):
