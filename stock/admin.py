e93b0f8fc4a3fb3c6f4340287a7cfb63ea86e7a6
Pavel Polinetskyi <pavel.polinetskyi@gmail.com>
2018-06-10 19:19:18 +0200

    [upd] Stock admin interface

--- stock/admin.py
+++ stock/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Category, Product, TransferTransaction, TopupTransaction, ProductTransaction, MiscTransaction
+from .models import (Category, Product, TransferTransaction, TopupTransaction, ProductTransaction,
+                     MiscTransaction, Barcode, StockRefill, ProductRefill)
 
 
 class ProductInline(admin.TabularInline):
@@ -8,16 +9,39 @@ class ProductInline(admin.TabularInline):
     extra = 1
 
 
+class BarcodeInline(admin.TabularInline):
+    model = Barcode
+    extra = 1
+
+
+class ProductRefillInline(admin.TabularInline):
+    model = ProductRefill
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price',)
+    list_display = ('name', 'category', 'price', 'stock_amount')
     search_fields = ('name',)
+    inlines = (BarcodeInline,)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     inlines = (ProductInline,)
+
+
+@admin.register(Barcode)
+class BarcodeAdmin(admin.ModelAdmin):
+    list_display = ("product", "code")
+    search_fields = ("product__name", "code")
+
+
+@admin.register(StockRefill)
+class StockRefillAdmin(admin.ModelAdmin):
+    list_display = ("user", "when")
+    inlines = (ProductRefillInline,)
 
 
 @admin.register(TransferTransaction)
