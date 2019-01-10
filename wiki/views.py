d2b5357629e1051104d443f648920e925dff1d2e
Herrgrim0 <rgrimau@ulb.ac.be>
2019-01-10 17:18:38 +0100

    [enh]

--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def wiki_home(request):
-    return render(request, "wiki_home.html")
+    return render(request, "wiki_home.html", {})
