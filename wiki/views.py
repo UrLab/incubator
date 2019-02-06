d917730b3f6bfaf7fbe346163289af83217650f4
Herrgrim0 <rgrimau@ulb.ac.be>
2019-02-06 22:28:53 +0100

    [add] add, edit, detail view for article

--- wiki/views.py
+++ wiki/views.py
@@ -1,5 +1,46 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView, UpdateView
+from users.mixins import PermissionRequiredMixin
+from datetime import datetime
+
+from .models import Article
+from .forms import ArticleForm
 
 def wiki_home(request):
     return render(request, "wiki_home.html", {})
+
+class ArticleAddView(PermissionRequiredMixin, CreateView):
+    form_class = ArticleForm
+    template_name = 'add_article.html'
+    permission_required = 'projects.add_project'
+
+    def get_initial(self):
+        return {
+            'creator': self.request.user,
+            'created': datetime.now(),
+            'nbr_revision': 0,
+        }
+
+    def form_valid(self, form):
+        ret = super(ArticleAddView, self).form_valid(form)
+        action.send(self.request.user, verb='a créé', action_object=self.object)
+
+        return ret
+
+class ArticleEditView(PermissionRequiredMixin, UpdateView):
+    form_class = ArticleForm
+    model = Article
+    template_name = 'add_article.html'
+    permission_required = 'projects.change_project'
+
+    def form_valid(self, form):
+        ret = super(ArticleEditView, self).form_valid(form)
+        action.send(self.request.user, verb='a édité', action_object=self.object)
+
+        return ret
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    context_object_name = 'article'
