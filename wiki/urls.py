26e15044f61e2dbb8bbdeac6a34ec9fde4058d8f
Herrgrim0 <rgrimau@ulb.ac.be>
2019-02-06 22:28:00 +0100

    [add] add, edit, view article

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
-from .views import (wiki_home,)
+from .views import (
+    wiki_home, ArticleDetailView, ArticleAddView, ArticleEditView)
 
 urlpatterns = [
     url(r'^$', wiki_home, name='wiki_home'),
+    url(r'^add$', ArticleAddView.as_view(), name='add_article'),
+    url(r'^edit/(?P<pk>[0-9]+)$', ArticleEditView.as_view(), name='edit_article'),
+    url(r'^(?P<pk>[0-9]+)$', ArticleDetailView.as_view(), name='view_article'),
 ]
