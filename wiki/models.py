cc9dca78b28f7d0e945864544a841182f2239916
Herrgrim0 <rgrimau@ulb.ac.be>
2019-02-06 22:27:10 +0100

    [add] created, last_modified & content field; class Meta; __str__

--- wiki/models.py
+++ wiki/models.py
@@ -1,15 +1,21 @@
 from django.db import models
+from datetime import datetime
 import reversion
 
 
 @reversion.register()
 class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Nom")
     creator = models.CharField(max_length=50)
-    title = models.CharField(max_length=200, default="Article")
-    article = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True, blank=True)
+    content = models.TextField(verbose_name="Contenu", blank=True)
     nbr_revision = models.IntegerField(default=0)
 
-    def save():
+    class Meta:
+        verbose_name = "Article"
+
+    def save(self, *args, **kwargs):
         # Declare a revision block.
         with reversion.create_revision():
 
@@ -20,3 +26,6 @@ class Article(models.Model):
             # Store some meta-information.
             reversion.set_user(request.user)
             reversion.set_comment("Created revision 1")
+
+    def __str__(self):
+        return self.title
