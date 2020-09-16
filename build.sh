3b3bdf52627d4e5981e3d36a52129785c8d3dcb3
Minigrim0 <florent.grimau@outlook.com>
2020-09-17 01:05:07 +0200

    [Add] statix root for static collection

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 git pull
 docker-compose up -d --build
 docker-compose exec web python manage.py migrate --noinput
-# docker-compose exec web python manage.py collectstatic --no-input --clear
+docker-compose exec web python manage.py collectstatic --no-input --clear
 docker-compose restart nginx
--- incubator/settings.py
+++ incubator/settings.py
@@ -149,6 +149,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static/")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
