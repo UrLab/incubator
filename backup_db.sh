895332eee4917647bae3b3e660fa7510e4c43f4e
Minigrim0 <florent.grimau@outlook.com>
2020-09-19 11:52:45 +0200

    [Fix] typo

--- backup_db.sh
+++ backup_db.sh
@@ -1,2 +1,2 @@
 mkdir -p /home/incubator/dumps/$(date +"%y-%m-%d")
-docker-compose -f /home/incubator/docker/app/incubator/docker-compose.yml exec db /usr/local/bin/pg_dump --dbname="incubator_db" --username="incubator" > /home/incubator/dumps/$(date +"%y-%m-%d")/incubator.dump
+docker-compose -f /home/incubator/docker/apps/incubator/docker-compose.yml exec db /usr/local/bin/pg_dump --dbname="incubator_db" --username="incubator" > /home/incubator/dumps/$(date +"%y-%m-%d")/incubator.dump
