85a314f4e9100fdf5624329989955b4ade5b0e24
Minigrim0 <florent.grimau@outlook.com>
2020-09-29 01:47:26 +0200

    [Upd] urls for django3

--- badges/urls.py
+++ badges/urls.py
@@ -1,17 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from .views import BadgeDetailView, promote_user,\
     BadgeHomeView, BadgeWearAddView
 
 urlpatterns = [
-    url(r'^$', BadgeHomeView.as_view(), name='badges_home_view'),
-    url(r'^add/(?P<pk>[\w-]+)/$', BadgeWearAddView, name='badges_add'),
-
-    url(r'^add/$', BadgeWearAddView, name='badges_add'),
-
-    url(r'^(?P<action>[\w-]+)/(?P<username>[\w-]+)/(?P<pk>[\w-]+)/{0,}$',
-        login_required(promote_user), name='promote_user'),
-
-    url(r'^(?P<pk>[\w-]+)/{0,}$',
-        login_required(BadgeDetailView.as_view()), name='badge_view'),
+    path('', BadgeHomeView.as_view(), name='badges_home_view'),
+    path('add/<int:pk>/', BadgeWearAddView, name='badges_add'),
+    path('add/', BadgeWearAddView, name='badges_add'),
+    path(
+        '<slug:action>/<str:username>/<int:pk>/', login_required(promote_user), name='promote_user'),
+    path(
+        '<int:pk>/', login_required(BadgeDetailView.as_view()), name='badge_view'),
 ]
