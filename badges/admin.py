4f6dcb4f099ce324cc0d964b97c0bed063d9ae12
Minigrim0 <florent.grimau@outlook.com>
2020-10-07 02:18:45 +0200

    [Fix] Flake8

--- badges/admin.py
+++ badges/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import Badge, BadgeWear
 
+
 @admin.register(Badge)
 class BadgeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name',)
+
 
 @admin.register(BadgeWear)
 class BadgeWearAdmin(admin.ModelAdmin):
