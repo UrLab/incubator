898d8c77a26e89f1a9c83ca7db2245c63b14d1c1
Minigrim0 <grimauflorent@gmail.com>
2020-12-08 14:57:13 +0100

    [Fix] back script

--- backup_db.sh
+++ backup_db.sh
@@ -1,2 +1,3 @@
 mkdir -p /home/incubator/dumps/$(date +"%y-%m-%d")
-docker-compose -f /home/incubator/docker/apps/incubator/docker-compose.yml exec db /usr/local/bin/pg_dump --dbname="incubator_db" --username="incubator" > /home/incubator/dumps/$(date +"%y-%m-%d")/incubator.dump
+docker-compose -f /home/incubator/docker/apps/incubator/docker-compose.yml exec db /usr/local/bin/pg_dump --dbname="incubator_db" --username="incubator" -f /backup.dump
+docker cp incubator_db_1:/backup.dump /home/incubator/dumps/$(date +"%y-%m-%d")/incubator.dump
