d44c62971c33296abbeb7a1a7b5fbc15c0fbd926
Minigrim0 <grimauflorent@gmail.com>
2021-10-18 19:55:07 +0200

    [Upd] Urls

--- badges/urls.py
+++ badges/urls.py
@@ -5,8 +5,8 @@ import badges.views as bviews
 urlpatterns = [
     path('', bviews.BadgeHomeView.as_view(), name='badges_home_view'),
     path('add/<int:pk>/', bviews.BadgeWearAddView, name='badges_add'),
-    path('add/', bviews.BadgeWearAddView, name='badges_add'),
-    path('<slug:action>/<str:username>/<int:pk>/', login_required(bviews.promote_user), name='promote_user'),
+    path('propose/', login_required(bviews.ProposeBadgeView.as_view()), name='badge_proposal'),
     path('<int:pk>/', login_required(bviews.BadgeDetailView.as_view()), name='badge_view'),
     path('<int:pk>/review', login_required(bviews.BadgeApproveView.as_view()), name='badge_review'),
+    path('<slug:action>/<str:username>/<int:pk>/', login_required(bviews.promote_user), name='promote_user'),
 ]
