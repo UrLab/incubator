c97b823c8b786a34e90f65ebd56cf85a3f6f9999
minigrim0 <grimauflorent@gmail.com>
2022-03-29 21:30:27 +0200

    [Upd] Serializer

--- streams/serializers.py
+++ streams/serializers.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 
 
 class UtripSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_price(self, obj):
+        return f"{obj.product.price}€"
+
     class Meta:
         model = ProductTransaction
-        fields = ('user__username', 'product__price')
+        fields = ('username', 'price')
