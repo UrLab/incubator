5f412bb8cfb6d8f377dcafb7aeaf749cd0d6c1e3
minigrim0 <grimauflorent@gmail.com>
2022-03-29 21:05:53 +0200

    [Add] api url

--- incubator/apiurls.py
+++ incubator/apiurls.py
@@ -9,19 +9,20 @@ import space.views
 
 
 api = routers.DefaultRouter()
-api.register(r'events', events.views.EventViewSet)
-api.register(r'meetings', events.views.MeetingViewSet)
-api.register(r'users', users.views.UserViewSet)
-api.register(r'projects', projects.views.ProjectViewSet)
-api.register(r'stock/categories', stock.views.CategoryViewSet)
-api.register(r'stock/products', stock.views.ProductViewSet)
-api.register(r'space/openings', space.views.OpeningsViewSet)
-api.register(r'space/pamela', space.views.PamelaViewSet, basename="pamela")
-api.register(r'space/motd', space.views.MotdViewSet)
+api.register('events', events.views.EventViewSet)
+api.register('meetings', events.views.MeetingViewSet)
+api.register('users', users.views.UserViewSet)
+api.register('projects', projects.views.ProjectViewSet)
+api.register('stock/categories', stock.views.CategoryViewSet)
+api.register('stock/products', stock.views.ProductViewSet)
+api.register('streams/utrip/', streams.api_views.UtripViewSet)
+api.register('space/openings', space.views.OpeningsViewSet)
+api.register('space/pamela', space.views.PamelaViewSet, basename="pamela")
+api.register('space/motd', space.views.MotdViewSet)
 
 
 urlpatterns = [
-    url(r'^hackeragenda', events.views.HackerAgendaAPI.as_view()),
-    url(r'^events/next_meeting', events.views.NextMeetingAPI.as_view()),
-    url(r'^', include(api.urls)),
+    url('^hackeragenda', events.views.HackerAgendaAPI.as_view()),
+    url('^events/next_meeting', events.views.NextMeetingAPI.as_view()),
+    url('^', include(api.urls)),
 ]
