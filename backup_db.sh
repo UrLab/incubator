6c43946beb55692a80c4e4da218b9efeb4fb7ffe
Minigrim0 <grimauflorent@gmail.com>
2022-10-23 16:34:33 +0200

    [Upd] Save medias too

--- backup_db.sh
+++ backup_db.sh
@@ -4,8 +4,16 @@ BACKUP_DIR=/home/$(USER)/dumps
 
 # Create the backup file
 mkdir -p /home/incubator/dumps/$(CURRENT_MONTH)
+
+# Dump the database
 docker-compose -f /home/incubator/docker/apps/incubator/docker-compose.yml exec db /usr/local/bin/pg_dump --dbname="incubator_db" --username="incubator" -f /backup.dump
+
+# Compress medias
+docker-compose -f /home/incubator/docker/apps/incubator/docker-compose.yml exec nginx /bin/bash -c "tar -czf /backup.tar.gz /home/app/web/media"
+
+# Copy the backup files from docker
 docker cp incubator_db_1:/backup.dump /home/incubator/dumps/$(CURRENT_MONTH)/$(CURRENT_DATE).dump
+docker cp incubator_nginx_1:/backup.tar.gz /home/incubator/dumps/$(CURRENT_MONTH)/media_$(CURRENT_DATE).tar.gz
 
 # Delete old backups (Keep for 90 days, all will be available on the second server)
 find /home/incubator/dumps/ -type f -mtime +90 -exec rm {} \;
@@ -20,3 +28,4 @@ fi
 # Copy the backup file to the secondary server
 ssh $(SECONDARY_SERVER_HOST) "mkdir -p /home/incubator/dumps/$(CURRENT_MONTH)"
 scp /home/incubator/dumps/$(CURRENT_MONTH)/$(CURRENT_DATE).dump $(SECONDARY_SERVER_HOST):/home/incubator/dumps/$(CURRENT_MONTH)/$(CURRENT_DATE).dump
+scp /home/incubator/dumps/$(CURRENT_MONTH)/media_$(CURRENT_DATE).tar.gz $(SECONDARY_SERVER_HOST):/home/incubator/dumps/$(CURRENT_MONTH)/media_$(CURRENT_DATE).tar.gz
