5711e4b145c8414fbb2f7d6408730e814d027dce
Minigrim0 <grimauflorent@gmail.com>
2023-04-17 19:21:44 +0200

    [Add] active field in products

--- stock/models.py
+++ stock/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
