1ebddc45711583a6251a54a754e3b7f111534298
Minigrim0 <grimauflorent@gmail.com>
2023-10-17 16:59:30 +0200

    [Fix] linting

--- incubator/management/commands/close_space.py
+++ incubator/management/commands/close_space.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 from space.djredis import get_redis, set_space_open, space_is_open
 
+
 class Command(BaseCommand):
     help = "Closes the space automatically everyday at 4a.m."
 
