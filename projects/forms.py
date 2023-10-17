b994af5cd83d38f85cc380f5b5f28f4abd241c7c
Minigrim0 <grimauflorent@gmail.com>
2023-10-17 16:20:39 +0200

    [Upd] clean form to avoid non https images

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,7 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Project, Comment
 
 
@@ -10,6 +13,18 @@ class ProjectForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 25}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        content = cleaned_data.get("content")
+        expr = re.compile(r"!\[.+\]\(http:\/\/.+\)")
+        matches = expr.findall(content)
+
+        if len(matches) > 0:
+            self.add_error(
+                "content",
+                f"The content of the article contains non https images ! ({' - '.join(matches)})"
+            )
 
 
 class CommentForm(forms.ModelForm):
